4c7353eb9e12d6a8097dd98c252a445f1dea03ac
Ricardo Taipe <rdavid28@hotmail.es>
2021-12-02 15:33:39 -0500

    initialize server

--- backend-api/src/index.js
+++ backend-api/src/index.js
@@ -1,2 +1,31 @@
 const express = require("express");
+const http = require("http");
+const path = require("path");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+
 const app = express();
+
+//Settings
+app.set("port", 3001 || process.env.PORT);
+
+//Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Custom error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ status: error.status, message: error.message });
+});
+
+let server = http.createServer(app);
+server.listen(app.get("port"), () => {
+  console.log("Server on port ", app.get("port"));
+});
+
+module.exports = server;
