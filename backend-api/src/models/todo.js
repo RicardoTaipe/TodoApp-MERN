f3a2edc255bf4a2a84250d5fd35fb945262f32ce
Ricardo Taipe <rdavid28@hotmail.es>
2021-12-02 17:09:33 -0500

    add todo mongoose schema

---
+++ backend-api/src/models/todo.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const TodoSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
+
+module.exports = model("Todo", TodoSchema);
