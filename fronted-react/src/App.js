7a24c590e0b4092a00fefd5675dca0d9a7904957
Ricardo Taipe <rdavid28@hotmail.es>
2022-10-05 19:36:50 -0500

    update frontend

--- fronted-react/src/App.js
+++ fronted-react/src/App.js
@@ -4,12 +4,18 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
+import Account from "./components/Account";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<Todo />} />
+          <Route path="account" element={<Account />} />
+          <Route path="todo" element={<Todo />} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
--- fronted-react/src/pages/Dashboard/index.js
+++ fronted-react/src/pages/Dashboard/index.js
@@ -15,9 +15,34 @@ import NotesIcon from "@mui/icons-material/Notes";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import Avatar from "@mui/material/Avatar";
 
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const drawerWidth = 240;
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("AuthToken");
+    navigate("/login");
+  };
+
+  const loadTodoPage = () => {
+    navigate("/todo");
+  };
+
+  const loadAccountPage = () => {
+    navigate("/account");
+  };
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("AuthToken");
+    if (authToken === null) {
+      navigate("/login");
+    }
+  });
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -61,21 +86,21 @@ const Dashboard = () => {
           </center>
           <Divider />
           <List>
-            <ListItem button key="Todo">
+            <ListItem button key="Todo" onClick={loadTodoPage}>
               <ListItemIcon>
                 <NotesIcon />
               </ListItemIcon>
               <ListItemText primary="Todo" />
             </ListItem>
 
-            <ListItem button key="Account">
+            <ListItem button key="Account" onClick={loadAccountPage}>
               <ListItemIcon>
                 <AccountBoxIcon />
               </ListItemIcon>
               <ListItemText primary="Account" />
             </ListItem>
 
-            <ListItem button key="Logout">
+            <ListItem button key="Logout" onClick={logoutHandler}>
               <ListItemIcon>
                 <ExitToAppIcon />
               </ListItemIcon>
@@ -86,35 +111,7 @@ const Dashboard = () => {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Outlet />
       </Box>
     </Box>
   );
